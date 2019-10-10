f07f37ba8076ba6113dcd7da022a7f395aa3a622
Fumi Kanari <expugnatiomundi@gmail.com>
2019-10-10 19:25:32 +0900

    almost complete jack parser

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -58,7 +58,7 @@ func (b *Base) WriteTerminal() {
 		value = "undefined token"
 	}
 
-	b.write(fmt.Sprintf("\t<%v>%v</%v>", token.Type(), value, token.Type()))
+	b.write(fmt.Sprintf("<%v> %v </%v>", token.Type(), value, token.Type()))
 	b.Tokenizer.Advance()
 }
 
@@ -107,24 +107,22 @@ func (b *Base) CompileSubroutineDec() {
 	b.WriteTerminal() // (
 	b.CompileParameterList()
 	b.WriteTerminal() // )
-
 	b.CompileSubroutineBody()
-
 	b.WriteCloseTag("subroutineDec")
+	b.CompileSubroutineDec()
 }
 
 // CompileParameterList -- write parameter list like (int x,int y)
 func (b *Base) CompileParameterList() {
-	if b.GetToken().Symbol() == ")" {
-		return
-	}
 	b.WriteOpenTag("parameterList")
-	b.WriteTerminal() // type
-	b.WriteTerminal() // varName
-	for b.GetToken().Symbol() == "," {
-		b.WriteTerminal() // ,
+	if b.GetToken().Symbol() != ")" {
 		b.WriteTerminal() // type
 		b.WriteTerminal() // varName
+		for b.GetToken().Symbol() == "," {
+			b.WriteTerminal() // ,
+			b.WriteTerminal() // type
+			b.WriteTerminal() // varName
+		}
 	}
 	b.WriteCloseTag("parameterList")
 }
@@ -160,21 +158,171 @@ func (b *Base) CompileVarDec() {
 
 // CompileStatements -- write statements like let if while do return
 func (b *Base) CompileStatements() {
-	switch b.GetToken().KeywordType() {
-	case tokenizer.Let:
-		//TODO compile let statements
-	case tokenizer.If:
-		//TODO compile if statements
-	case tokenizer.While:
-		//TODO compile while statements
-	case tokenizer.Do:
-		//TODO compile do statements
-	case tokenizer.Return:
-		//TODO compile return statements
-	default:
-		return
+	b.WriteOpenTag("statements")
+
+STATEMENTS_LOOP:
+	for {
+		switch b.GetToken().KeywordType() {
+		case tokenizer.Let:
+			b.CompileLet()
+		case tokenizer.If:
+			b.CompileIf()
+		case tokenizer.While:
+			b.CompileWhile()
+		case tokenizer.Do:
+			b.CompileDo()
+		case tokenizer.Return:
+			b.CompileReturn()
+		default:
+			break STATEMENTS_LOOP
+		}
 	}
+
+	b.WriteCloseTag("statements")
+}
+
+// CompileLet -- write let statements
+func (b *Base) CompileLet() {
+	b.WriteOpenTag("letStatement")
+	b.WriteTerminal() // let
+	b.WriteTerminal() // varName
+	if b.GetToken().Symbol() == "[" {
+		b.WriteTerminal() // [
+		b.CompileExpression()
+		b.WriteTerminal() // ]
+	}
+	b.WriteTerminal() // =
+	b.CompileExpression()
+	b.WriteTerminal() // ;
+	b.WriteCloseTag("letStatement")
+}
+
+// CompileIf -- write if statements
+func (b *Base) CompileIf() {
+	b.WriteOpenTag("ifStatement")
+	b.WriteTerminal() // if
+	b.WriteTerminal() // (
+	b.CompileExpression()
+	b.WriteTerminal() // )
+	b.WriteTerminal() // {
 	b.CompileStatements()
+	b.WriteTerminal() // }
+	if b.GetToken().KeywordType() == tokenizer.Else {
+		b.WriteTerminal() // else
+		b.WriteTerminal() // {
+		b.CompileStatements()
+		b.WriteTerminal() // }
+	}
+	b.WriteCloseTag("ifStatement")
+}
+
+// CompileWhile -- write while statements
+func (b *Base) CompileWhile() {
+	b.WriteOpenTag("whileStatement")
+	b.WriteTerminal() // while
+	b.WriteTerminal() // (
+	b.CompileExpression()
+	b.WriteTerminal() // )
+	b.WriteTerminal() // {
+	b.CompileStatements()
+	b.WriteTerminal() // }
+	b.WriteCloseTag("whileStatement")
+}
+
+// CompileDo -- write do statementes
+func (b *Base) CompileDo() {
+	b.WriteOpenTag("doStatement")
+	b.WriteTerminal() // do
+	b.WriteTerminal() // subroutine name, class name , var name
+	switch b.GetToken().Symbol() {
+	case "(":
+		b.WriteTerminal() // (
+		b.CompileExpressionList()
+		b.WriteTerminal() // )
+	case ".":
+		b.WriteTerminal() // .
+		b.WriteTerminal() // subroutine name
+		b.WriteTerminal() // (
+		b.CompileExpressionList()
+		b.WriteTerminal() // )
+	}
+	b.WriteTerminal() // ;
+	b.WriteCloseTag("doStatement")
+}
+
+// CompileReturn -- write return statements
+func (b *Base) CompileReturn() {
+	b.WriteOpenTag("returnStatement")
+	b.WriteTerminal() // return
+	if b.GetToken().Symbol() != ";" {
+		b.CompileExpression()
+	}
+	b.WriteTerminal() // ;
+	b.WriteCloseTag("returnStatement")
+}
+
+// CompileExpression -- write expressioon
+func (b *Base) CompileExpression() {
+	b.WriteOpenTag("expression")
+	b.CompileTerm()
+	for isOperand(b.GetToken()) {
+		b.WriteTerminal() // write operand
+		b.CompileTerm()
+	}
+	b.WriteCloseTag("expression")
+}
+
+// CompileTerm -- write term
+func (b *Base) CompileTerm() {
+	b.WriteOpenTag("term")
+	defer b.WriteCloseTag("term")
+
+	switch b.GetToken().Symbol() {
+	case "-", "~":
+		b.WriteTerminal() // - or ~
+		b.CompileTerm()
+		return
+	case "(":
+		b.WriteTerminal() // (
+		b.CompileExpression()
+		b.WriteTerminal() // )
+		return
+	default:
+		b.WriteTerminal()
+	}
+
+	switch b.GetToken().Symbol() {
+	case "[":
+		b.WriteTerminal() // [
+		b.CompileExpression()
+		b.WriteTerminal() // ]
+		return
+	// subroutine call
+	case "(":
+		b.WriteTerminal() // (
+		b.CompileExpressionList()
+		b.WriteTerminal() // )
+	case ".":
+		b.WriteTerminal() // .
+		b.WriteTerminal() // subrourine name
+		b.WriteTerminal() // (
+		b.CompileExpressionList()
+		b.WriteTerminal() // )
+	}
+
+}
+
+// CompileExpressionList -- compile a bunch of expressions
+func (b *Base) CompileExpressionList() {
+	b.WriteOpenTag("expressionList")
+	if b.GetToken().Symbol() != ")" {
+		b.CompileExpression()
+		for b.GetToken().Symbol() == "," {
+			b.WriteTerminal()
+			b.CompileExpression()
+		}
+	}
+	b.WriteCloseTag("expressionList")
 }
 
 func isClassVarDec(t *tokenizer.Token) bool {
@@ -189,6 +337,15 @@ func isClassVarDec(t *tokenizer.Token) bool {
 func isSubroutineDec(t *tokenizer.Token) bool {
 	switch t.KeywordType() {
 	case tokenizer.Constructor, tokenizer.Function, tokenizer.Method:
+		return true
+	default:
+		return false
+	}
+}
+
+func isOperand(t *tokenizer.Token) bool {
+	switch t.Symbol() {
+	case "+", "-", "*", "/", "&amp;", "|", "&lt;", "&gt;", "=":
 		return true
 	default:
 		return false
